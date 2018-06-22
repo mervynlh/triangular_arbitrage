76173c0d74b8d49a880ba8afe57eb45e8f7c24d1
mervynlh <mervyn_lh@163.com>
2018-06-22 15:07:36 +0800

    mes

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
     name="triangular_arbitrage",
 
@@ -10,7 +12,7 @@ setup(
 
     url='http://github.com/mervynlh/triangular_arbitrage',
 
-    author='mervyn',
+    author='mervynlh',
     author_email='mervyn_lh@163.com',
 
     license='MIT',
@@ -18,5 +20,4 @@ setup(
     keywords='block chain triangular arbitrage',
 
     packages=find_packages(exclude=['triangular_arbitrage']),
-
 )
