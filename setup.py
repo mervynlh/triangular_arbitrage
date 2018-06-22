1cd02c0953f2bfec2b4aedea20a572216393e0f2
mervynlh <mervyn_lh@163.com>
2018-06-22 15:25:59 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup, find_packages
+import os
+import requests
+
+
+# 将markdown格式转换为rst格式
+def md_to_rst(from_file, to_file):
+    r = requests.post(url='http://c.docverter.com/convert',
+                      data={'to': 'rst', 'from': 'markdown'},
+                      files={'input_files[]': open(from_file, 'rb')})
+    if r.ok:
+        with open(to_file, "wb") as f:
+            f.write(r.content)
+
+md_to_rst("README.md", "README.rst")
+
+long_description = 'block chain triangular arbitrage'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setup(
     name="triangular_arbitrage",
 
-    version='0.1.1',
+    version='0.1.2',
 
     description='block chain triangular arbitrage',
 
