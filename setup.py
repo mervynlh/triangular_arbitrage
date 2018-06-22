0e00b8399191aa3b268f7b48f14875b61e8030cf
mervynlh <mervyn_lh@163.com>
2018-06-22 16:44:22 +0800

    0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="triangular-arbitrage",
 
-    version='0.0.1',
+    version='0.0.2',
 
     description='block chain triangular arbitrage',
 
