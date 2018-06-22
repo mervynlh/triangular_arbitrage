dd218bcc75333669622159210078971e0e85a826
mervynlh <mervyn_lh@163.com>
2018-06-22 14:17:19 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="triangular_arbitrage",
+
+    version='0.1',
+
+    description='block chain triangular arbitrage',
+
+    url='http://github.com/mervynlh/triangular_arbitrage',
+
+    author='mervyn',
+    author_email='mervyn_lh@163.com',
+
+    license='MIT',
+
+    keywords='block chain triangular arbitrage',
+
+    packages=find_packages(exclude=['triangular_arbitrage']),
+
+    install_requires=['pandas'],
+
+)
